02a56400ff3dc76654ccce5f813230c288ce1bbf
valentinnsh <valentinnsh@gmail.com>
2020-06-03 21:29:40 +0300

    Calc nodes coords for planet47mb.osm

--- calc_basics.py
+++ calc_basics.py
@@ -4,12 +4,17 @@ import saveload as sl
 #buildings = xmlparser.getBuildings()
 #firestations = xmlparser.getFireStations()
 #roads = xmlparser.getRoads()
+coords = xmlparser.getNodesCoords()
+
 
 #sl.save_obj(firestations, "firestations")
 #sl.save_obj(buildings, "buildings")
 #sl.save_obj(roads, "roads")
+sl.save_obj(coords, "coords")
 
 #fs = sl.load_obj("firestations")
 #bld = sl.load_obj("buildings")
-rd = sl.load_obj("roads")
-print(len(rd))
+#rd = sl.load_obj("roads")
+co = sl.load_obj("coords")
+
+print(len(co))
