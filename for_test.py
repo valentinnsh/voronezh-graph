eec8ae1103344d06af5cd6d89fb78b615f9b2321
AWedASTera <awed234@mail.ru>
2020-06-05 15:35:34 +0300

    add getInfo using vertex idm test dijkstra on csv

--- for_test.py
+++ for_test.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import graph
 import csv
 import test
-df = pd.read_csv('export_test2.csv')
+df = pd.read_csv('export_test.csv')
 fc_list=list(df.iloc[:,0])
 df = df.drop([df.columns[0]], axis=1)
 df.to_csv('file_1.csv', index=False)
@@ -14,4 +15,12 @@ data = list(reader)
 print(data)
 print(fc_list)
 d = test.formGraphList(fc_list,data)
-print(d)
+print('gas!gas!gas!')
+(D,Parent) = graph.Dijkstra(d,'0')
+f_list = [str(node) for node in fc_list]
+f_list.remove('0')
+tree = graph.GetTree(f_list, Parent)
+print("tree from b_id[0] (one of the objects) to a_id (sample of houses):")    
+print(tree)
+print("length of previous tree:")
+print(graph.GetSumOfTree(tree,d))
